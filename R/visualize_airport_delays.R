b4dda886e03e8bdd3e7fc2ab8022a6a4b2579262
Uğurcan Laçin <ugurcanlacin@gmail.com>
2017-10-10 12:21:30 +0200

    added visualization

--- R/visualize_airport_delays.R
+++ R/visualize_airport_delays.R
@@ -8,9 +8,23 @@
 visualize_airport_delays = function(){
   requireNamespace("nycflights13")
   requireNamespace("dplyr")
+  library(dplyr)
+  library(ggplot2)
   flights <- nycflights13::flights
   airports <- nycflights13::airports
+  associatedData <- inner_join(flights,airports,by = c("dest" = "faa"))
   
+  subsettedData <- dplyr::select(associatedData,dest,lat,lon,arr_delay)
+  
+  meansWithLatLon <- subsettedData %>%
+    group_by(dest,lat,lon) %>%
+    summarize(Mean = mean(arr_delay,na.rm = TRUE))
+  
+  p <- ggplot(meansWithLatLon, aes(x = dest, y = Mean, label = dest)) +
+    geom_point() + labs(x = "Airports") + theme_bw()
+  
+  return(p)
 }
 
 # visualize_airport_delays()
+
